5cb18f09cdbb1922b2f46adaa8843ec6b23e1d03
Daniel Billotte <daniel@litfuze.com>
2022-10-11 03:05:46 -0700

    deleted old files, one of which was breaking the build

--- proj_scripts.sh
+++
@@ -1,6 +0,0 @@
-# intended for heavy.lang.json and heavy.ir.json, to list out all of the language blocks/commands
-alias hlang_blocks="grep '\".*\": {' | sort | uniq | cut -d '\"' -f 2"
-
-# invoke tox with alt config file, 2nd one include test coverage stats
-alias toxx="tox -c ~/projects/hvcc/dgb-hvcc/tox.dev.ini"
-alias toxc="tox -c ~/projects/hvcc/dgb-hvcc/tox.dev.ini -- --cov-config=tox.ini --cov=hvcc"
