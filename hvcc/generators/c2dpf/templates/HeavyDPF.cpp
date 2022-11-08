80a8407686a7eaa7c4a2690034d5e4f517369302
dreamer <dreamer@puikheid.nl>
2022-11-08 22:51:09 +0100

    use correct frame counter

--- hvcc/generators/c2dpf/templates/HeavyDPF.cpp
+++ hvcc/generators/c2dpf/templates/HeavyDPF.cpp
@@ -243,48 +243,48 @@ void {{class_name}}::handleMidiInput(uint32_t frames, const MidiEvent* midiEvent
     int dataSize = *(&midiEvents[i].data + 1) - midiEvents[i].data;
 
     for (int i = 0; i < dataSize; ++i) {
-      _context->sendMessageToReceiverV(HV_HASH_MIDIIN, 1000.0*timePos.frame/getSampleRate(), "ff",
+      _context->sendMessageToReceiverV(HV_HASH_MIDIIN, 1000.0*midiEvents->frame/getSampleRate(), "ff",
         (float) midiEvents[i].data[i],
         (float) channel);
     }
 
     if(mrtSet.find(status) != mrtSet.end())
     {
-      _context->sendMessageToReceiverV(HV_HASH_MIDIREALTIMEIN, 1000.0*timePos.frame/getSampleRate(),
+      _context->sendMessageToReceiverV(HV_HASH_MIDIREALTIMEIN, 1000.0*midiEvents->frame/getSampleRate(),
         "ff", (float) status);
     }
 
     // typical midi messages
     switch (command) {
       case 0x80: {  // note off
-        _context->sendMessageToReceiverV(HV_HASH_NOTEIN, 1000.0*timePos.frame/getSampleRate(), "fff",
+        _context->sendMessageToReceiverV(HV_HASH_NOTEIN, 1000.0*midiEvents->frame/getSampleRate(), "fff",
           (float) data1, // pitch
           (float) 0, // velocity
           (float) channel);
         break;
       }
       case 0x90: { // note on
-        _context->sendMessageToReceiverV(HV_HASH_NOTEIN, 1000.0*timePos.frame/getSampleRate(), "fff",
+        _context->sendMessageToReceiverV(HV_HASH_NOTEIN, 1000.0*midiEvents->frame/getSampleRate(), "fff",
           (float) data1, // pitch
           (float) data2, // velocity
           (float) channel);
         break;
       }
       case 0xB0: { // control change
-        _context->sendMessageToReceiverV(HV_HASH_CTLIN, 1000.0*timePos.frame/getSampleRate(), "fff",
+        _context->sendMessageToReceiverV(HV_HASH_CTLIN, 1000.0*midiEvents->frame/getSampleRate(), "fff",
           (float) data2, // value
           (float) data1, // cc number
           (float) channel);
         break;
       }
       case 0xC0: { // program change
-        _context->sendMessageToReceiverV(HV_HASH_PGMIN, 1000.0*timePos.frame/getSampleRate(), "ff",
+        _context->sendMessageToReceiverV(HV_HASH_PGMIN, 1000.0*midiEvents->frame/getSampleRate(), "ff",
           (float) data1,
           (float) channel);
         break;
       }
       case 0xD0: { // aftertouch
-        _context->sendMessageToReceiverV(HV_HASH_TOUCHIN, 1000.0*timePos.frame/getSampleRate(), "ff",
+        _context->sendMessageToReceiverV(HV_HASH_TOUCHIN, 1000.0*midiEvents->frame/getSampleRate(), "ff",
           (float) data1,
           (float) channel);
         break;
@@ -292,7 +292,7 @@ void {{class_name}}::handleMidiInput(uint32_t frames, const MidiEvent* midiEvent
       case 0xE0: { // pitch bend
         // combine 7bit lsb and msb into 32bit int
         hv_uint32_t value = (((hv_uint32_t) data2) << 7) | ((hv_uint32_t) data1);
-        _context->sendMessageToReceiverV(HV_HASH_BENDIN, 1000.0*timePos.frame/getSampleRate(), "ff",
+        _context->sendMessageToReceiverV(HV_HASH_BENDIN, 1000.0*midiEvents->frame/getSampleRate(), "ff",
           (float) value,
           (float) channel);
         break;
