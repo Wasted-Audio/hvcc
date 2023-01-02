b1314aaf8a71bda68f2acd7cdc36b5b7b016d667
dreamer <alexander@puikheid.nl>
2023-01-02 23:28:26 +0100

    add tests for midi input objects; add polytouch in and out

--- hvcc/generators/c2dpf/templates/HeavyDPF.cpp
+++ hvcc/generators/c2dpf/templates/HeavyDPF.cpp
@@ -9,6 +9,7 @@
 
 #define HV_HASH_NOTEIN          0x67E37CA3
 #define HV_HASH_CTLIN           0x41BE0f9C
+#define HV_HASH_POLYTOUCHIN     0xBC530F59
 #define HV_HASH_PGMIN           0x2E1EA03D
 #define HV_HASH_TOUCHIN         0x553925BD
 #define HV_HASH_BENDIN          0x3083F0F7
@@ -17,6 +18,7 @@
 
 #define HV_HASH_NOTEOUT         0xD1D4AC2
 #define HV_HASH_CTLOUT          0xE5e2A040
+#define HV_HASH_POLYTOUCHOUT    0xD5ACA9D1
 #define HV_HASH_PGMOUT          0x8753E39E
 #define HV_HASH_TOUCHOUT        0x476D4387
 #define HV_HASH_BENDOUT         0xE8458013
@@ -267,6 +269,13 @@ void {{class_name}}::handleMidiInput(uint32_t frames, const MidiEvent* midiEvent
         _context->sendMessageToReceiverV(HV_HASH_NOTEIN, 1000.0*midiEvents->frame/getSampleRate(), "fff",
           (float) data1, // pitch
           (float) data2, // velocity
+          (float) channel);
+        break;
+      }
+      case 0xA0: { // polyphonic aftertouch
+        context->sendMessageToReceiverV(HV_HASH_POLYTOUCHIN, 1000.0*midiEvents->frame/getSampleRate(), , "fff",
+          (float) data2, // pressure
+          (float) data1, // note
           (float) channel);
         break;
       }
