cafe7d478c0550ca80ecc6d541ecf3d81185ab7f
dreamer <dreamer@puikheid.nl>
2023-02-03 15:25:15 +0100

    fix mypy error

--- hvcc/__init__.py
+++ hvcc/__init__.py
@@ -135,7 +135,7 @@ def generate_extern_info(hvir: Dict, results: Dict):
 def compile_dataflow(
     in_path: str,
     out_dir: str,
-    patch_name: Optional[str] = None,
+    patch_name: str = "heavy",
     patch_meta_file: Optional[str] = None,
     search_paths: Optional[List] = None,
     generators: Optional[List] = None,
@@ -165,7 +165,7 @@ def compile_dataflow(
                 except Exception as e:
                     return add_error(results, f"Unable to open json_file: {e}")
 
-    patch_name = patch_name or patch_meta.get("name", "heavy")  
+    patch_name = patch_meta.get("name", patch_name)
     generators = ["c"] if generators is None else [x.lower() for x in generators]
 
     if in_path.endswith((".pd")):
@@ -321,7 +321,6 @@ def main():
     parser.add_argument(
         "-n",
         "--name",
-        default="heavy",
         help="Provides a name for the generated Heavy context.")
     parser.add_argument(
         "-m",
