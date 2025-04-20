bc7100afbf3b67d5a0b16b3f4979e3eaa5e4f86b
dreamer <dreamer@puikheid.nl>
2025-04-20 14:34:35 +0200

    add tests for basic math operators

--- tests/test_control_expr.py
+++ tests/test_control_expr.py
@@ -23,17 +23,56 @@ class TestPdControlExprPatches(TestPdControlBase):
     SCRIPT_DIR = os.path.dirname(__file__)
     TEST_DIR = os.path.join(os.path.dirname(__file__), "pd", "control_expr")
 
+    def test_tilde(self):
+        """ only supports integers """
+        self._test_control_patch("test-tilde.pd")
+
     def test_mult(self):
         self._test_control_patch("test-mult.pd")
 
     def test_div(self):
         self._test_control_patch("test-div.pd")
 
+    def test_modulo(self):
+        self._test_control_patch("test-modulo.pd")
+
+    def test_add_sub(self):
+        self._test_control_patch("test-add-sub.pd")
+
+    def test_shift(self):
+        self._test_control_patch("test-shift.pd")
+
     def test_lt_gt(self):
         self._test_control_patch("test-lt-gt.pd")
 
+    def test_lte_gte(self):
+        self._test_control_patch("test-lte-gte.pd")
+
+    # not yet supported
+    # def test_eq_neq(self):
+    #     self._test_control_patch("test-eq-neq.pd")
+
+    def test_bit_and(self):
+        self._test_control_patch("test-bit-and.pd")
+
+    def test_exc_or(self):
+        self._test_control_patch("test-exc-or.pd")
+
+    def test_bit_or(self):
+        self._test_control_patch("test-bit-or.pd")
+
+    def test_log_and(self):
+        self._test_control_patch("test-log-and.pd")
+
+    def test_log_or(self):
+        self._test_control_patch("test-log-or.pd")
+
     def test_cos(self):
         self._test_control_patch("test-cos.pd")
+
+    # not yet supported
+    # def test_if(self):
+    #     self._test_control_patch("test-if.pd")
 
 
 def main():
