700986700e37ec4781342a0c0a4f23c03e6506b8
dreamer <dreamer@puikheid.nl>
2025-09-21 17:35:39 +0200

    fix HvControlPack; use correct number of arguments in soundfiler; use DPF getSpecialDir(); split to separate template include

--- hvcc/generators/c2dpf/templates/HeavyDPF.cpp
+++ hvcc/generators/c2dpf/templates/HeavyDPF.cpp
@@ -2,6 +2,8 @@
 
 #include "Heavy_{{name}}.h"
 #include "{{class_name}}.hpp"
+#include "DistrhoPluginUtils.hpp"
+#include <string>
 #include <set>
 {% if meta.denormals is sameas false %}
 #include "extra/ScopedDenormalDisable.hpp"
@@ -179,141 +181,7 @@ void {{class_name}}::setOutputParameter(uint32_t sendHash, const HvMessage *m)
   {%- endif %}
 }
 
-void HeavyDPF_soundfilermock::sndFileOperator(uint32_t sendHash, const HvMessage *m)
-{
-  switch (sendHash) {
-    case HV_HASH_SND_READ: // __hv_snd_read
-    {
-      float sndID = hv_msg_getFloat(m, 0);
-      const char *fileName = hv_msg_getSymbol(m, 1);
-      const char *tableName = hv_msg_getSymbol(m, 2);
-
-      char sndRecInfo[32];
-      char sndRecSamples[32];
-      char *bufInfo = sndRecInfo;
-      char *bufSamples = sndRecSamples;
-
-      snprintf(bufInfo, 31, "%d__hv_snd_info", (int) sndID);
-      snprintf(bufSamples, 31, "%d__hv_snd_samples", (int) sndID);
-
-      hv_uint32_t tableHash = hv_string_to_hash(tableName);
-      float *table = _context->getBufferForTable(tableHash);
-      int tableSize = _context->getLengthForTable(tableHash);
-
-      TinyWav tw;
-      tinywav_open_read(&tw, fileName, TW_INLINE);
-
-      tinywav_read_f(&tw, table, tableSize);
-
-      _context->sendMessageToReceiverV(
-        hv_string_to_hash(sndRecInfo), 0, "ffffs",
-        (float) tw.h.SampleRate,  // sample rate
-        44.0,                     // header size
-        1.0,                      // channels
-        (float) tw.sampFmt,       // bytes per sample
-        "l"                       // endianness
-      );
-
-      _context->sendFloatToReceiver(
-        hv_string_to_hash(sndRecSamples),
-        float(tableSize)
-      );
-
-      tinywav_close_read(&tw);
-
-      break;
-    }
-    case HV_HASH_SND_READ_RES: // __hv_snd_read_resize
-    {
-      float sndID = hv_msg_getFloat(m, 0);
-      const char *fileName = hv_msg_getSymbol(m, 1);
-      const char *tableName = hv_msg_getSymbol(m, 2);
-
-      char sndRecInfo[32];
-      char sndRecSamples[32];
-      char *bufInfo = sndRecInfo;
-      char *bufSamples = sndRecSamples;
-
-      snprintf(bufInfo, 31, "%d__hv_snd_info", (int) sndID);
-      snprintf(bufSamples, 31, "%d__hv_snd_samples", (int) sndID);
-
-      hv_uint32_t tableHash = hv_string_to_hash(tableName);
-
-      TinyWav tw;
-      tinywav_open_read(&tw, fileName, TW_INLINE);
-      int tableSize = tw.numFramesInHeader;
-
-      _context->setLengthForTable(tableHash, (uint32_t)tableSize);
-      float *table = _context->getBufferForTable(tableHash);
-
-      tinywav_read_f(&tw, table, tableSize);
-
-      _context->sendMessageToReceiverV(
-        hv_string_to_hash(sndRecInfo), 0, "ffffs",
-        (float) tw.h.SampleRate,  // sample rate
-        44.0,                     // header size
-        1.0,                      // channels
-        (float) tw.sampFmt,       // bytes per sample
-        "l"                       // endianness
-      );
-
-      _context->sendFloatToReceiver(
-        hv_string_to_hash(sndRecSamples),
-        float(tableSize)
-      );
-
-      tinywav_close_read(&tw);
-
-      break;
-    }
-    case HV_HASH_SND_WRITE: // __hv_snd_write
-    {
-      float sndID = hv_msg_getFloat(m, 0);
-      const char *fileName = hv_msg_getSymbol(m, 1);
-      const char *tableName = hv_msg_getSymbol(m, 2);
-
-      char sndRecInfo[32];
-      char sndRecSamples[32];
-      char *bufInfo = sndRecInfo;
-      char *bufSamples = sndRecSamples;
-
-      snprintf(bufInfo, 31, "%d__hv_snd_info", (int) sndID);
-      snprintf(bufSamples, 31, "%d__hv_snd_samples", (int) sndID);
-
-      hv_uint32_t tableHash = hv_string_to_hash(tableName);
-      float *table = _context->getBufferForTable(tableHash);
-      int tableSize = _context->getLengthForTable(tableHash);
-
-      TinyWav tw;
-      tinywav_open_write(
-        &tw, 1,
-        _context->getSampleRate(),
-        TW_FLOAT32, TW_INLINE,
-        fileName
-      );
-      tinywav_write_f(&tw, table, tableSize);
-
-      _context->sendMessageToReceiverV(
-        hv_string_to_hash(sndRecInfo), 0, "ffffs",
-        (float) tw.h.SampleRate,  // sample rate
-        44.0,                     // header size
-        1.0,                      // channels
-        (float) tw.sampFmt,       // bytes per sample
-        "l"                       // endianness
-      );
-
-      _context->sendFloatToReceiver(
-        hv_string_to_hash(sndRecSamples),
-        float(tableSize)
-      );
-
-      tinywav_close_write(&tw);
-
-      break;
-    }
-    default: break;
-  }
-}
+{% include 'sndFileOperator.cpp' %}
 
 // -------------------------------------------------------------------
 // Process
