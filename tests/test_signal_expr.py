37cd7b3d0c926846547e32489487bbcf2e14e8f0
dreamer <dreamer@puikheid.nl>
2025-09-28 18:38:29 +0200

    add isnan

--- tests/test_signal_expr.py
+++ tests/test_signal_expr.py
@@ -111,8 +111,8 @@ class TestPdControlExprPatches(TestPdControlBase):
     def test_finite(self):
         self._test_control_patch("test-finite.pd")
 
-    # def test_isnan(self):
-    #     self._test_control_patch("test-isnan.pd")
+    def test_isnan(self):
+        self._test_control_patch("test-isnan.pd")
 
     # def test_copysign(self):
     #     self._test_control_patch("test-copysign.pd")
