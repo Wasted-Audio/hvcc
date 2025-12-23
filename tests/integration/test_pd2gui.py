d038daefb97b1d205f8d6ee3c220431b37bb077b
dreamer <dreamer@puikheid.nl>
2025-12-23 15:48:42 +0100

    set version in tests

--- tests/integration/test_pd2gui.py
+++ tests/integration/test_pd2gui.py
@@ -4,8 +4,10 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 import os
+import json
 
 from hvcc.interpreters.pd2gui.pd2gui import pd2gui
+from hvcc.version import VERSION
 
 
 class TestPdGuiParser:
@@ -25,11 +27,12 @@ class TestPdGuiParser:
 
         gui_path = os.path.join(self.SCRIPT_DIR, "ir", os.path.splitext(path)[0] + ".gui.json")
         with open(gui_path, "r") as f:
-            gui = f.read()
+            gui = json.loads(f.read())
 
         expected_path = os.path.join(os.path.splitext(source_path)[0] + ".golden.json")
         with open(expected_path, "r") as f:
-            expected = f.read()
+            expected = json.loads(f.read())
+            expected['version'] = VERSION
 
         assert gui == expected
 
