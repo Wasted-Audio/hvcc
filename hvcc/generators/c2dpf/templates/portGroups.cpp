00b062766b5b9fc06131592b4bb18f12f96dd1ae
dreamer <dreamer@puikheid.nl>
2026-01-29 17:40:27 +0100

    actual typo

--- hvcc/generators/c2dpf/templates/portGroups.cpp
+++ hvcc/generators/c2dpf/templates/portGroups.cpp
@@ -61,10 +61,10 @@ void {{class_name}}::initAudioPort(bool input, uint32_t index, AudioPort& port)
         port.name   = "Output Right";
         port.symbol = "out_right";
         break;
-      }
       port.groupId = kPortGroupStereo;
   {%- endif %}
 {%- endif %}
+      }
     }
 }
 
