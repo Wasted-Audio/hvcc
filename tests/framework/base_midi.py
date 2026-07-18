09b2e41582d2ffbcf605fd2eaedb46d22d23c724
dreamer <dreamer@puikheid.nl>
2026-07-18 12:05:39 +0200

    add midi-out tests; fix midi-out objects when packing values

--- tests/framework/base_midi.py
+++ tests/framework/base_midi.py
@@ -26,6 +26,7 @@ from tests.framework.base_test import HvBaseTest
 
 
 class TestPdMIDIBase(HvBaseTest):
+    MIDI_RUNNER_SOURCE = "test_midi.cpp"
 
     def compile_and_run(
         self,
@@ -104,7 +105,7 @@ class TestPdMIDIBase(HvBaseTest):
         # copy over additional C assets
         assert out_dir
         c_src_dir = Path(out_dir, "c")
-        shutil.copy2(Path(self.SCRIPT_DIR, "src/test_midi.cpp"), c_src_dir)
+        shutil.copy2(Path(self.SCRIPT_DIR, f"src/{self.MIDI_RUNNER_SOURCE}"), c_src_dir)
         shutil.copy2(Path(self.SCRIPT_DIR, "src/midifile/include/MidiFile.h"), c_src_dir)
         shutil.copy2(Path(self.SCRIPT_DIR, "src/midifile/include/MidiEventList.h"), c_src_dir)
         shutil.copy2(Path(self.SCRIPT_DIR, "src/midifile/include/MidiEvent.h"), c_src_dir)
@@ -130,33 +131,33 @@ class TestPdMIDIBase(HvBaseTest):
                 # NO SIMD (always test this case)
                 result = self.compile_and_run(c_sources, out_dir, num_iterations, "HV_SIMD_NONE")
                 message = fail_message or \
-                    self.create_fail_message("".join(result), "".join(golden), "HV_SIMD_NONE")
+                    self.create_fail_message(result, golden, "HV_SIMD_NONE")
                 self.assertEqual(result, golden, message)
 
                 if platform.machine().startswith("x86"):
                     # SSE
                     result = self.compile_and_run(c_sources, out_dir, num_iterations, "HV_SIMD_SSE")
                     message = fail_message or \
-                        self.create_fail_message("".join(result), "".join(golden), "HV_SIMD_SSE")
+                        self.create_fail_message(result, golden, "HV_SIMD_SSE")
                     self.assertEqual(result, golden, message)
 
                     # SSE with FMA
                     result = self.compile_and_run(c_sources, out_dir, num_iterations, "HV_SIMD_SSE_FMA")
                     message = fail_message or \
-                        self.create_fail_message("".join(result), "".join(golden), "HV_SIMD_SSE_FMA")
+                        self.create_fail_message(result, golden, "HV_SIMD_SSE_FMA")
                     self.assertEqual(result, golden, message)
 
                     # AVX (with FMA)
                     result = self.compile_and_run(c_sources, out_dir, num_iterations, "HV_SIMD_AVX")
                     message = fail_message or \
-                        self.create_fail_message("".join(result), "".join(golden), "HV_SIMD_AVX")
+                        self.create_fail_message(result, golden, "HV_SIMD_AVX")
                     self.assertEqual(result, golden, message)
 
                 elif platform.machine().startswith("arm"):
                     # NEON
                     result = self.compile_and_run(c_sources, out_dir, num_iterations, "HV_SIMD_NEON")
                     message = fail_message or \
-                        self.create_fail_message("".join(result), "".join(golden), "HV_SIMD_NEON")
+                        self.create_fail_message(result, golden, "HV_SIMD_NEON")
                     self.assertEqual(result, golden, message)
 
         else:
