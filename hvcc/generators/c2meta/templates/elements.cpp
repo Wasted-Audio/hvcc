e6dde52929640d1a3cdc98380b0d9f402eb90966
dreamer <1185977+dromer@users.noreply.github.com>
2026-08-17 20:23:00 +0200

    generate elements.cpp and generate panel+assets if not present in meta.json (#407)
    * generate elements.cpp and generate panel+assets if not present in meta.json
    
    * divide coords by 2; generate module screenshot
    
    * scale panel and scale audio i/o

--- hvcc/generators/c2meta/templates/elements.cpp
+++ hvcc/generators/c2meta/templates/elements.cpp
@@ -5,90 +5,69 @@
 #include "CoreModules/register_module.hh"
 #include "HeavyMetaModule_{{name}}.hpp"
 
-
-struct {{name}}MMInfo : MetaModule::ModuleInfoBase {
-  static constexpr std::string_view slug{"{{ name }}"};
-  static constexpr std::string_view description{"{{ meta.description }}"};
-  static constexpr uint32_t width_hp = 10;
-  static constexpr std::string_view png_filename{"{{ name }}/simple_gain.png"};
-
-  {% raw %}
-  static constexpr std::array<MetaModule::Element, 6> Elements{{
-    MetaModule::Knob{{{{{20, 20, MetaModule::Coords::Center, "Vol"}, "tester/components/knob.png"}}, 0.5f}},
-    MetaModule::JackInput{{{{10, 60, MetaModule::Coords::Center, "Input1"}, "tester/components/jack.png"}}},
-    MetaModule::JackInput{{{{30, 60, MetaModule::Coords::Center, "Input2"}, "tester/components/jack.png"}}},
-    MetaModule::JackOutput{{{{10, 80, MetaModule::Coords::Center, "Output1"}, "tester/components/jack.png"}}},
-    MetaModule::JackOutput{{{{30, 80, MetaModule::Coords::Center, "Output2"}, "tester/components/jack.png"}}},
-    MetaModule::MonoLight{{{{20, 35, MetaModule::Coords::Center, "Vol LED"}, "tester/components/led.png"}}}
-  }};
-  {% endraw %}
-};
+{%- set assets = meta.modules[0].assets %}
+{%- set num_elements = assets.knobs|length + assets.leds|length + assets.inputs|length + assets.outputs|length %}
+{%- set ns = namespace(counter=0) %}
+{%- set ratio = 240/128.5 %}
 
 void init_{{ name|lower }}() {
-  MetaModule::register_module<{{ name }}MM, {{name}}MMInfo>("{{ meta['brand'] }}");
+  static std::array<MetaModule::Element, {{num_elements}}> elements;
+  static std::array<ElementCount::Indices, {{num_elements}}> indices;
+
+{% for knob in assets.knobs %}
+  MetaModule::Knob {{knob.param}};
+  {{knob.param}}.x_mm = {{((knob.coords.x/ratio)|round(2))}};
+  {{knob.param}}.y_mm = {{((knob.coords.y/ratio)|round(2))}};
+  {{knob.param}}.coords = MetaModule::Coords::TopLeft;
+  {{knob.param}}.image = "{{name}}/components/{{knob.image.name}}";
+  {{knob.param}}.short_name = "{{knob.param}}";
+  elements[{{ns.counter}}] = {{knob.param}};
+  indices[{{ns.counter}}] = {.param_idx = {{knob.param|capitalize}}ID};
+  {% set ns.counter = ns.counter + 1 %}
+{%- endfor %}
+
+{%- for input in assets.inputs %}
+  MetaModule::JackInput injack{{input.id}};
+  injack{{input.id}}.x_mm = {{((input.coords.x/ratio)|round(2))}};
+  injack{{input.id}}.y_mm = {{((input.coords.y/ratio)|round(2))}};
+  injack{{input.id}}.coords = MetaModule::Coords::TopLeft;
+  injack{{input.id}}.image = "{{name}}/components/{{input.image.name}}";
+  injack{{input.id}}.short_name = "Input {{input.id}}";
+  elements[{{ns.counter}}] = injack{{input.id}};
+  indices[{{ns.counter}}] = {.input_idx = InputID{{input.id}}};
+  {% set ns.counter = ns.counter + 1 %}
+{%- endfor %}
+
+{%- for output in assets.outputs %}
+  MetaModule::JackOutput outjack{{output.id}};
+  outjack{{output.id}}.x_mm = {{((output.coords.x/ratio)|round(2))}};
+  outjack{{output.id}}.y_mm = {{((output.coords.y/ratio)|round(2))}};
+  outjack{{output.id}}.coords = MetaModule::Coords::TopLeft;
+  outjack{{output.id}}.image = "{{name}}/components/{{output.image.name}}";
+  outjack{{output.id}}.short_name = "Output {{output.id}}";
+  elements[{{ns.counter}}] = outjack{{output.id}};
+  indices[{{ns.counter}}] = {.output_idx = OutputID{{output.id}}};
+  {% set ns.counter = ns.counter + 1 %}
+{%- endfor %}
+
+{%- for led in assets.leds %}
+  MetaModule::MonoLight led{{led.led}};
+  led{{led.led}}.x_mm = {{((led.coords.x/ratio)|round(2))}};
+  led{{led.led}}.y_mm = {{((led.coords.y/ratio)|round(2))}};
+  led{{led.led}}.coords = MetaModule::Coords::TopLeft;
+  led{{led.led}}.image = "{{name}}/components/{{led.image.name}}";
+  led{{led.led}}.short_name = "LED {{led.led}}";
+  elements[{{ns.counter}}] = led{{led.led}};
+  indices[{{ns.counter}}] = {.light_idx = {{led.led|capitalize}}ID};
+  {% set ns.counter = ns.counter + 1 %}
+{%- endfor %}
+
+  MetaModule::ModuleInfoView info{
+      .description = "{{ meta.description }}",
+      .width_hp = {{ (assets.panel.size.x / 7.5) | round | int }},
+      .elements = elements,
+      .indices = indices,
+  };
+
+  MetaModule::register_module<{{ name }}MM>("{{ meta.name }}", "{{ name }}", info, "{{name}}/{{ assets.panel.image.name }}");
 }
-
-// void init_{{ name|lower }}() {
-//   // This is a simple way to create a ModuleInfoView
-//   // See the README for better ways when dealing with more elements.
-
-//   static std::array<MetaModule::Element, 6> elements;
-//   static std::array<ElementCount::Indices, 6> indices;
-
-//   MetaModule::Knob gain;
-//   gain.x_mm = 20;
-//   gain.y_mm = 20;
-//   gain.image = "tester/components/knob.png";
-//   gain.short_name = "Gain";
-//   elements[0] = gain;
-//   indices[0] = {.param_idx = VolID};
-
-//   MetaModule::JackInput injack1;
-//   injack1.x_mm = 10;
-//   injack1.y_mm = 60;
-//   injack1.image = "tester/components/jack.png";
-//   injack1.short_name = "Input";
-//   elements[1] = injack1;
-//   indices[1] = {.input_idx = InputID0};
-
-//   MetaModule::JackInput injack2;
-//   injack2.x_mm = 30;
-//   injack2.y_mm = 60;
-//   injack2.image = "tester/components/jack.png";
-//   injack2.short_name = "Input";
-//   elements[2] = injack2;
-//   indices[2] = {.input_idx = InputID1};
-
-//   MetaModule::JackOutput outjack1;
-//   outjack1.x_mm = 10;
-//   outjack1.y_mm = 80;
-//   outjack1.image = "tester/components/jack.png";
-//   outjack1.short_name = "Output";
-//   elements[3] = outjack1;
-//   indices[3] = {.output_idx = OutputID0};
-
-//   MetaModule::JackOutput outjack2;
-//   outjack2.x_mm = 30;
-//   outjack2.y_mm = 80;
-//   outjack2.image = "tester/components/jack.png";
-//   outjack2.short_name = "Output";
-//   elements[4] = outjack2;
-//   indices[4] = {.output_idx = OutputID1};
-
-//   MetaModule::MonoLight light;
-//   light.x_mm = 20;
-//   light.y_mm = 35;
-//   light.image = "tester/components/led.png";
-//   light.short_name = "Gain LED";
-//   elements[5] = light;
-//   indices[5] = {.light_idx = LightID};
-
-//   MetaModule::ModuleInfoView info{
-//       .description = "{{ meta['description'] }}",
-//       .width_hp = 10,
-//       .elements = elements,
-//       .indices = indices,
-//   };
-
-//   MetaModule::register_module<{{ name }}MM>("{{ name }}", "{{ name }}", info, "{{ name }}/simple_gain.png");
-// }
