81ed80ac0962f6678515c807c29a95c2789d5fde
dreamer <dreamer@puikheid.nl>
2026-08-21 07:21:01 +0200

    use memalign

--- hvcc/generators/c2meta/static/compat.cpp
+++ hvcc/generators/c2meta/static/compat.cpp
@@ -2,13 +2,13 @@
 #include <cstddef>
 
 struct _reent;
+extern void *memalign(size_t align, size_t size);
 
 extern "C" {
 
 void* _memalign_r(struct _reent* r, size_t alignment, size_t bytes) {
     (void)r;
-    (void)alignment;
-    return std::malloc(bytes);
+    return memalign(alignment, bytes);
 }
 
 }
